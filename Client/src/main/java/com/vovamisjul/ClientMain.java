ab9f0ba3072bcb87a5ca3e2435987c1d986a68e8
vovamisjul <vovamisjul@gmail.com>
2019-08-27 14:07:05 +0300

    Add simple client and server

--- Client/src/main/java/com/vovamisjul/ClientMain.java
+++ Client/src/main/java/com/vovamisjul/ClientMain.java
@@ -1,3 +1,5 @@
+package com.vovamisjul;
+
 public class ClientMain {
     public static void main(String[] args) {
         System.out.println("hi client");
---
+++ Server/src/main/java/com/vovamisjul/ServerMain.java
@@ -0,0 +1,7 @@
+package com.vovamisjul;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        System.out.println("hi Server");
+    }
+}
