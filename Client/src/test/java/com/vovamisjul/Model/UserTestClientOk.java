3642635361fb68b5d4af055202569f6d80223357
vovamisjul <vovamisjul@gmail.com>
2019-08-28 12:37:54 +0300

    add tests

--- Client/src/test/java/com/vovamisjul/Model/UserTestClientOk.java
+++ Client/src/test/java/com/vovamisjul/Model/UserTestClientOk.java
@@ -11,7 +11,7 @@ public class UserTestClientOk {
     public void start() {
         InputStream in = new ByteArrayInputStream("127.0.0.1\r\n1111\r\n/adsasdads\r\n/register client vasya\r\nhi\r\n/exit\r\n\r\n".getBytes());
         System.setIn(in);
-        OutputStream output = new ByteArrayOutputStream();
+        OutputStream output = new ByteArrayOutputStream(1000);
         OutputStream expected = new ByteArrayOutputStream(1000);
         try {
             expected.write(("Enter ip address and port of server\r\nEnter /register [role] [name] to start\r\nThere are no such command\r\nPrint your message to start\r\nYour interlocutor ends dialog. Press enter to exit\r\n").getBytes());
@@ -21,6 +21,11 @@ public class UserTestClientOk {
         PrintStream out = new PrintStream(output);
         System.setOut(out);
         new User().start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         assertEquals(expected.toString(), output.toString());
     }
 }
